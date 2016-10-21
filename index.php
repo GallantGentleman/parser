12ef8702867c34761a501567d544e0910c43911e
GallantGentleman <webcrash@mail.ua>
2016-10-21 03:57:08 +0300

    Parser Done

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
 <?php
-require_once("phpQuery.php");
+header("Content-Type: text/html;charset=utf-8");
 
-$url = file_get_contents("http://belwooddoors.ru/doors");
+require_once("simple_html_dom.php");
 
+$url = "page1.html";
+
+$html = new simple_html_dom();
+$html = file_get_html($url);
+
+$items = array();
+$count = 0;
+
+if ($html->innertext != '' and count($html->find('.product-grid'))) {
+	foreach($html->find('div.one') as $div) {
+		$items[$count]['title'] = $div->find('div.name', 0)->plaintext;
+		$items[$count]['description'] = trim($div->find('div.description', 0)->plaintext);
+		$items[$count]['price'] = trim($div->find('div.price', 0)->plaintext);
+		$items[$count]['image'] = $div->find('div.image a', 0)->first_child()->src;
+		$items[$count]['url'] = $div->find('div.image a', 0)->href;
+
+		$count++;
+	}
+
+} else {
+	echo "Can't find element";
+} 
+
+echo "<pre>";
+echo print_r($items);
+echo "</pre>";
